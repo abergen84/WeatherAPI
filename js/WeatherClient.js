d9bab8c5fbcd705fad0437b1c86ffd7275f55f79
Adam Bergen <bergen@gmail.com>
2014-11-10 16:17:14 -0600

    cleaned up to include backbone, but doesnt work properly..no errors popping up

---
+++ js/WeatherClient.js
@@ -0,0 +1,206 @@
+(function(window, undefined){
+	"use strict";
+
+
+//var options = {api_key: "251a327b65c598a3b6842fa35513c058"};
+
+
+function WeatherClient(){
+        
+    
+        // if(!options.api_key){
+        //     throw new Error ("You need a proper API key");
+        // }
+
+        // this.cities = {Houston: "29.7628,-95.3831", Seattle: "47.6097,-122.3331", LA: "34.0500,-118.2500"};
+        // this.base_url = "https://api.forecast.io/forecast/";
+        // this.api_key = options.api_key;
+        // this.latitude = "29.7628";
+        // this.longitude = "-95.3831";
+        // this.complete_url = this.base_url + this.api_key + "/" + this.latitude + "," + this.longitude + "?callback=?";
+
+        // console.log(this.complete_url);
+
+        this.weatherCollection = new WeatherListings();
+
+        this.Routing();
+
+    }
+
+var WeatherView = Backbone.View.extend({
+	tagname: "div",
+	className: "weather",
+
+	initalize: function(opts){
+
+		this.options = _.extend(
+			{},
+			{ $container: $('body') },
+			opts
+			);
+
+		this.options.$container.append(this.el);
+
+		this.render();
+	},
+
+	template: "<h6>{currently.summary}</h6>",
+	
+	render: function(){		
+		this.el.innerHTML = _.template(this.template, this.model);
+	}
+
+});
+
+var WeatherListing = Backbone.Model.extend({
+
+	initalize: function(opts){
+
+		this.view = new WeatherView({model: this});
+	}
+});
+
+var WeatherListings = Backbone.Collection.extend({
+
+	CreateInputObject: function(){
+		var input = {};
+		$(':input').each(function(){
+			input = this.value;
+		});
+		console.log(input);
+
+		return input;
+	},
+
+	model: WeatherListing,
+
+	apikey: "251a327b65c598a3b6842fa35513c058",
+
+	cities: {Houston: "29.7628,-95.3831", Seattle: "47.6097,-122.3331", LA: "34.0500,-118.2500"},
+
+	url: function(){
+
+		var input = this.CreateInputObject();
+
+		return [
+			"https://api.forecast.io/forecast/",
+			this.apikey,
+			"/",
+			this.cities[input],
+			"?callback=?"
+		].join('');
+	},
+
+	parse: function(data){
+		return data.results;
+	}
+});
+
+//var listings = new weatherListings();
+
+WeatherClient.prototype.Routing = function(){
+
+        var self = this;
+
+        Path.map("#/results").to(function(){
+                
+                self.weatherCollection.fetch();
+        });
+
+    Path.root("#/");
+    Path.listen();
+
+    };
+
+
+window.WeatherClient = WeatherClient;
+
+}) (window, undefined);
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    // Weather.prototype.pullWeatherData = function() {
+    //     "use strict";
+
+    //     var input = this.createInputArray();
+
+    //     console.log(input);
+    //     console.log(this.cities);
+
+    //     return $.getJSON(this.base_url + this.api_key + "/" + this.cities[input] + "?callback=?").then(function(data){
+            
+    //         console.log(data);
+            
+    //         return data;
+    //     });
+    // };
+
+    // Weather.prototype.loadTemplate = function(template){
+    //     "use strict";
+
+    //     return $.get("./templates/" + template + ".html").then(function(htmlString){
+    //         return htmlString;
+    //     });
+    // };
+
+
+    // Weather.prototype.drawWeatherData = function(data, html){
+    //     "use strict";
+
+    //     document.querySelector('#weather').innerHTML = 
+    //             _.template(html, data);
+    // };
+
+    // Weather.prototype.createInputArray = function(){
+    //     "use strict";
+
+    //     var input = [];
+    //     $(':input').each(function(){
+    //         input = this.value;
+    //     });
+    //     console.log(input);
+    //     return input;
+    // };
+
+    // Weather.prototype.compareInputToCities = function(){
+    //     "use strict";
+
+    //     //var x = this.createInputArray();
+
+    //     cities.city.forEach(function(x){
+    //         if(x === cities.city){
+    //             console.log(x);
+    //             return x;
+    //         } else {
+    //             console.alert("This city does not exist in the database");
+    //         }
+    //     });
+    // };
+
+ 
+
+
+
+
+
+    //Notes:
+    //
+    //What i want this mini project to do:
+    //
+    //When you input a city in form, have it compare to a list of pre-set major cities defined at beginning and spit the weather out based on that city (i.e. based on latitude/longitude)
+    //Based on the weather, spit out a picture of a sun, cloudy, rain/thunder, etc.
+    //And lastly, make it look pretty.
+
+
+    
+    
--- js/app.js
+++ js/app.js
@@ -8,126 +8,17 @@
         loader.load(
             {url: "./bower_components/jquery/dist/jquery.min.js"},
             {url: "./bower_components/lodash/dist/lodash.min.js"},
-            {url: "./bower_components/pathjs/path.min.js"}
+            {url: "./bower_components/backbone/backbone.js"},
+            {url: "./bower_components/pathjs/path.min.js"},
+            {url: "./js/WeatherClient.js"}
         ).then(function(){
             _.templateSettings.interpolate = /{([\s\S]+?)}/g;
 
             // start app?
-        var options = {api_key: "251a327b65c598a3b6842fa35513c058"};
-
-        var rain = new Weather(options);
-
+        
+        var client = new WeatherClient();
 
         });
     }
-    
-    function Weather(options){
-        "use strict";
-    
-        if(!options.api_key){
-            throw new Error ("You need a proper API key");
-        }
-
-        this.cities = {Houston: "29.7628,-95.3831", Seattle: "47.6097,-122.3331", LA: "34.0500,-118.2500"};
-        this.base_url = "https://api.forecast.io/forecast/";
-        this.api_key = options.api_key;
-        this.latitude = "29.7628";
-        this.longitude = "-95.3831";
-        this.complete_url = this.base_url + this.api_key + "/" + this.latitude + "," + this.longitude + "?callback=?";
-
-        console.log(this.complete_url);
-
-
-        this.Routing();
-
-    }
-
-    Weather.prototype.pullWeatherData = function() {
-        "use strict";
-
-        var input = this.createInputArray();
-
-        console.log(input);
-        console.log(this.cities);
-
-        return $.getJSON(this.base_url + this.api_key + "/" + this.cities[input] + "?callback=?").then(function(data){
-            
-            console.log(data);
-            
-            return data;
-        });
-    };
-
-    Weather.prototype.loadTemplate = function(template){
-        "use strict";
-
-        return $.get("./templates/" + template + ".html").then(function(htmlString){
-            return htmlString;
-        });
-    };
-
-
-    Weather.prototype.drawWeatherData = function(data, html){
-        "use strict";
-
-        document.querySelector('#weather').innerHTML = 
-                _.template(html, data);
-    };
-
-    Weather.prototype.createInputArray = function(){
-        "use strict";
-
-        var input = [];
-        $(':input').each(function(){
-            input = this.value;
-        });
-        console.log(input);
-        return input;
-    };
-
-    // Weather.prototype.compareInputToCities = function(){
-    //     "use strict";
-
-    //     //var x = this.createInputArray();
-
-    //     cities.city.forEach(function(x){
-    //         if(x === cities.city){
-    //             console.log(x);
-    //             return x;
-    //         } else {
-    //             console.alert("This city does not exist in the database");
-    //         }
-    //     });
-    // };
-
-    Weather.prototype.Routing = function(){
-        "use strict";
-
-        var self = this;
-
-        Path.map("#/results").to(function(){
-                $.when(self.pullWeatherData(),
-                       self.loadTemplate("weather")
-                ).then(function(){
-                       self.drawWeatherData(arguments[0], arguments[1]);
-                });
-        });
-
-    Path.root("#/");
-    Path.listen();
-
-    };
-
-
-
-    //Notes:
-    //
-    //What i want this mini project to do:
-    //
-    //When you input a city in form, have it compare to a list of pre-set major cities defined at beginning and spit the weather out based on that city (i.e. based on latitude/longitude)
-    //Based on the weather, spit out a picture of a sun, cloudy, rain/thunder, etc.
-    //And lastly, make it look pretty.
-
-
     
     
